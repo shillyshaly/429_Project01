45474469dcaef8ed6a86499ae642f92f10ebf94c
shillyshaly <litoxl@hotmail.com>
2022-03-09 00:23:24 -0800

    still need to figure out request

--- main.go
+++ main.go
@@ -3,24 +3,19 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
 	"net"
 	"os"
 	"strings"
 )
 
-func handleError(err error) {
-	if err != nil {
-		fmt.Println("ERROR: " + err.Error())
-		os.Exit(2)
-	}
-}
-
-func testShit() {
-	ln, err := net.Listen("tcp", ":8080")
+func main() {
+	listen, err := net.Listen("tcp", ":8080")
 	handleError(err)
+	defer listen.Close()
 
 	for {
-		conn, err := ln.Accept()
+		conn, err := listen.Accept()
 		handleError(err)
 		go handleConnection(conn)
 	}
@@ -29,14 +24,63 @@ func testShit() {
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
-	for {
-		msg, _ := bufio.NewReader(conn).ReadString(' ')
-		msg = strings.TrimSuffix(msg, "\n")
-		fmt.Println("fuck: " + msg)
+	req, _ := parseRequest(conn)
+	dir, _ := generateResponse(req)
+	sendResponse(dir)
+}
+
+type request struct {
+	method string // GET, POST, etc.
+	//header textproto.MIMEHeader
+	body     []byte
+	uri      string // The raw URI from the request
+	protocol string // "HTTP/1.1"
+}
+
+func parseRequest(conn net.Conn) (*request, error) {
+	b, _, err := bufio.NewReader(conn).ReadLine()
+	req := new(request)
+	handleError(err)
+
+	sp := strings.Split(string(b), " ")
+	req.method, req.uri, req.protocol = sp[0], sp[1], sp[2]
+
+	fmt.Println("Method: " + req.method)
+	fmt.Println("URI: " + req.uri)
+	fmt.Println("Protocol: " + req.protocol)
+
+	return req, nil
+}
+
+func generateResponse(req *request) (string, error) {
+	files, err := ioutil.ReadDir("www")
+	handleError(err)
+
+	var dir string
+
+	for _, file := range files {
+		if req.method == "GET" || req.method == "HEAD" {
+			if strings.Contains("/"+file.Name(), req.uri) {
+				dir = file.Name()
+			}
+		}
 	}
+	return dir, nil
+}
+
+func sendResponse(dir string) {
 
 }
 
-func main() {
-	testShit()
+func handleError(err error) {
+	if err != nil {
+		fmt.Println("ERROR: " + err.Error())
+		os.Exit(2)
+	}
 }
+
+//accept()  done
+//parseRequest()  done
+//generateResponse(___)
+//sendResponse()
+//close()
